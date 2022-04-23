927a7fab90d75139dde734535abd4ada811e0d12
roman.pendrak <roman.pendrak@gmail.com>
2022-04-22 23:39:50 -0400

    added 3rd solution using strictly camel DSL

--- src/main/java/com/datagrate/exercise1/routes/Route1.java
+++ src/main/java/com/datagrate/exercise1/routes/Route1.java
@@ -18,12 +18,34 @@ public class Route1 extends RouteBuilder {
 		public void configure() throws Exception {
 			restConfiguration().component("servlet").port(8080).host("localhost").bindingMode(RestBindingMode.json);
 
-			// solution 1 using processor
-			rest().get("processor/getUserById/").produces(MediaType.APPLICATION_JSON_VALUE).route().process(processor).endRest();	
+			// solution 1 using processor - opens file using FileReader/JSONParsers utils
+			rest().get("processor/getUserById/")
+				.produces(MediaType.APPLICATION_JSON_VALUE)
+				.route()
+				.process(processor)
+				.endRest();	
 			
-			// solution 2 using strictly camel DSL 
-			rest().get("/getUserById").produces(MediaType.APPLICATION_JSON_VALUE).route().setProperty("userid", simple("${headers.userid}"))
-				.pollEnrich("file:userData/?fileName=userData.json&noop=true&idempotent=false", 5000).setBody().jsonpath("$.users..[?(@.userid==${header.userid})]").endRest();
+			// solution 2 using strictly camel DSL - using pollEnrich to load file into message 
+			rest().get("camel-dsl/getUserById")
+				.produces(MediaType.APPLICATION_JSON_VALUE)
+				.route()
+				.setProperty("userid", simple("${headers.userid}"))
+				.pollEnrich("file:userData/?fileName=userData.json&noop=true&idempotent=false", 5000)
+				.setBody().jsonpath("$.users..[?(@.userid==${exchangeProperty.userid})]")
+				.endRest();
+			
+			// solution 3 using strictly camel DSL - using simple language to read file resource and route to extractUserData endpoint
+			rest().get("camel-dsl-simple/getUserById")
+				.produces(MediaType.APPLICATION_JSON_VALUE)
+				.route()
+				.setProperty("userid", simple("${headers.userid}"))
+				.setBody().simple("resource:file:userData/userData.json")
+				.to("direct-vm:extractUserData");
+			
+			from("direct-vm:extractUserData")
+				.setBody()
+				.jsonpath("$.users..[?(@.userid==${exchangeProperty.userid})]")
+				.endRest();
 
 		}
 }
